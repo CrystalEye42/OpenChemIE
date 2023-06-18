c233e59ddbfff646964a8f59ca716e42c649ed6a
Alex Wang <alexwang7470@gmail.com>
2023-06-18 01:59:43 -0400

    fix bug with cleaning bboxes

--- chemescribe/utils.py
+++ chemescribe/utils.py
@@ -17,18 +17,6 @@ def get_figures_from_pages(pages, pdfparser):
             })
     return figures
 
-def get_overlap(bbox1, bbox2):
-    x1, y1, x2, y2 = bbox1
-    u1, v1, u2, v2 = bbox2
-    intersection_area = (min(u2,x2)-max(u1,x1))*(min(v2,y2)-max(v1,y1))
-    return intersection_area/((x2-x1)*(y2-y1)+(u2-u1)*(v2-v1)-intersection_area)
-
-def is_unique_bbox(bbox, bboxes):
-    for b in bboxes:
-        if get_overlap(b, bbox) > 0.9:
-            return False
-    return True
-
 def clean_bbox_output(figures, bboxes):
     results = []
     cropped = []
@@ -36,26 +24,23 @@ def clean_bbox_output(figures, bboxes):
     for i, output in enumerate(bboxes):
         mol_bboxes = [elt['bbox'] for elt in output if elt['category'] == '[Mol]']
         mol_scores = [elt['score'] for elt in output if elt['category'] == '[Mol]']
-        unique_bboxes = []
         data = {}
         results.append(data)
         data['image'] = figures[i]
         data['molecules'] = []
         for bbox, score in zip(mol_bboxes, mol_scores):
-            if is_unique_bbox(bbox, unique_bboxes):
-                unique_bboxes.append(bbox)
-                x1, y1, x2, y2 = bbox
-                height, width, _ = figures[i].shape
-                cropped_img = figures[i][int(y1*height):int(y2*height),int(x1*width):int(x2*width)]
-                cur_mol = {
-                    'bbox': bbox,
-                    'score': score,
-                    'image': cropped_img,
-                    #'info': None,
-                }
-                cropped.append(cropped_img)
-                data['molecules'].append(cur_mol)
-                references.append(cur_mol)
+            x1, y1, x2, y2 = bbox
+            height, width, _ = figures[i].shape
+            cropped_img = figures[i][int(y1*height):int(y2*height),int(x1*width):int(x2*width)]
+            cur_mol = {
+                'bbox': bbox,
+                'score': score,
+                'image': cropped_img,
+                #'info': None,
+            }
+            cropped.append(cropped_img)
+            data['molecules'].append(cur_mol)
+            references.append(cur_mol)
     return results, cropped, references
     
 def convert_to_pil(image):
